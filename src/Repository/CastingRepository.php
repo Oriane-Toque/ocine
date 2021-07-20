6a7b10b7d84720c21e9d4fcd77eaa041de84806c
Oriane-Toque <oriane.toque@gmail.com>
2021-07-20 15:41:34 +0200

    request DQL & QueryBuilder to find all casting + inner person by movie

--- src/Repository/CastingRepository.php
+++ src/Repository/CastingRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Casting;
+use App\Entity\Movie;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -22,6 +23,48 @@ class CastingRepository extends ServiceEntityRepository
     // /**
     //  * @return Casting[] Returns an array of Casting objects
     //  */
+
+		/**
+		 * Récupère tous les castings d'un film donné
+		 * Avec DQL
+		 *
+		 * @param Movie[] $movie
+		 * @return Casting[]
+		 */
+		public function findAllByMovieOrderCredit(Movie $movie): array
+    {
+			$entityManager = $this->getEntityManager();
+
+			$query = $entityManager->createQuery(
+					'SELECT c, p
+					FROM App\Entity\Casting c
+					INNER JOIN c.person p
+					WHERE c.movie = :movie
+					ORDER BY c.creditOrder ASC'
+			)->setParameter('movie', $movie);
+
+			return $query->getResult();
+    }
+
+		/**
+		 * Récupère tous les castings d'un film donné
+		 * Avec Query Builder
+		 *
+		 * @param Movie[] $movie
+		 * @return Casting[]
+		 */
+		public function findAllByMovieOrderCreditQb(Movie $movie): array
+    {
+        return $this->createQueryBuilder('c')
+						->innerJoin('c.person', 'p')
+            ->andWhere('c.movie = :movie')
+            ->setParameter('movie', $movie)
+            ->orderBy('c.creditOrder', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
     /*
     public function findByExampleField($value)
     {
