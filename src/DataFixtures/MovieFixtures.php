a01d821a194bdf12f6f6d7e0453597d401625398
Oriane-Toque <oriane.toque@gmail.com>
2021-07-20 12:11:15 +0200

    random generate genres's movie from MovieFixtures

--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -17,17 +17,33 @@ class MovieFixtures extends Fixture implements DependentFixtureInterface
 
 				for($nbMovie = 1; $nbMovie <= 100; $nbMovie++) {
 
-					// récupération des références dont on a besoin
-					$genre = $this->getReference("genre_".$faker->numberBetween(1, 12));
-
 					$movie = new Movie();
 					$movie->setTitle($faker->streetName());
 					$movie->setCreatedAt(new DateTime());
-					$movie->setReleaseDate(new DateTime());
+					$movie->setReleaseDate($faker->dateTimeBetween('-30 years', 'now'));
 					$movie->setDuration($faker->numberBetween(55, 180));
 
-					$movie->addGenre($genre);
+					/* Génère aléatoirement un certain nombre de genres par film soit 1 genre soit 2 */
+					if($movie->getReleaseDate() === $faker->dateTimeBetween('-20years', 'now')) {
 
+						for($nbGenre = 1; $nbGenre <= 3; $nbGenre++) {
+							// récupération des références dont on a besoin
+							$genre = $this->getReference("genre_".$faker->numberBetween(1, 12));
+
+							$movie->addGenre($genre);
+						} 
+
+					} else {
+
+						for($nbGenre = 1; $nbGenre <= 2; $nbGenre++) {
+							// récupération des références dont on a besoin
+							$genre = $this->getReference("genre_".$faker->numberBetween(1, 12));
+
+							$movie->addGenre($genre);
+						} 						
+					}
+					
+					
 					$manager->persist($movie);
 
 					// on enregistre le film dans une référence
