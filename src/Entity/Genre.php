ff09bfec967a48dfb50d7b32cfe30018673160cf
Oriane-Toque <oriane.toque@gmail.com>
2021-07-23 09:37:07 +0200

    relations & migrations job, department, team

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -12,69 +12,69 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Genre
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+	/**
+	 * @ORM\Id
+	 * @ORM\GeneratedValue
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
 
-    /**
-     * @ORM\Column(type="string", length=26)
-     */
-    private $name;
+	/**
+	 * @ORM\Column(type="string", length=26)
+	 */
+	private $name;
 
-    /**
-     * @ORM\ManyToMany(targetEntity=Movie::class, mappedBy="genres")
-     */
-    private $movies;
+	/**
+	 * @ORM\ManyToMany(targetEntity=Movie::class, mappedBy="genres")
+	 */
+	private $movies;
 
-    public function __construct()
-    {
-        $this->movies = new ArrayCollection();
-    }
+	public function __construct()
+	{
+		$this->movies = new ArrayCollection();
+	}
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
 
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
+	public function getName(): ?string
+	{
+		return $this->name;
+	}
 
-    public function setName(string $name): self
-    {
-        $this->name = $name;
+	public function setName(string $name): self
+	{
+		$this->name = $name;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @return Collection|Movie[]
-     */
-    public function getMovies(): Collection
-    {
-        return $this->movies;
-    }
+	/**
+	 * @return Collection|Movie[]
+	 */
+	public function getMovies(): Collection
+	{
+		return $this->movies;
+	}
 
-    public function addMovie(Movie $movie): self
-    {
-        if (!$this->movies->contains($movie)) {
-            $this->movies[] = $movie;
-            $movie->addGenre($this);
-        }
+	public function addMovie(Movie $movie): self
+	{
+		if (!$this->movies->contains($movie)) {
+			$this->movies[] = $movie;
+			$movie->addGenre($this);
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function removeMovie(Movie $movie): self
-    {
-        if ($this->movies->removeElement($movie)) {
-            $movie->removeGenre($this);
-        }
+	public function removeMovie(Movie $movie): self
+	{
+		if ($this->movies->removeElement($movie)) {
+			$movie->removeGenre($this);
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 }
---
+++ src/Entity/Job.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JobRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=JobRepository::class)
+ */
+class Job
+{
+	/**
+	 * @ORM\Id
+	 * @ORM\GeneratedValue
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
+
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $name;
+
+	/**
+	 * @ORM\Column(type="datetime")
+	 */
+	private $createdAt;
+
+	/**
+	 * @ORM\Column(type="datetime", nullable=true)
+	 */
+	private $updatedAt;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity=Department::class, inversedBy="jobs")
+	 * @ORM\JoinColumn(nullable=false)
+	 */
+	private $department;
+
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
+
+	public function getName(): ?string
+	{
+		return $this->name;
+	}
+
+	public function setName(string $name): self
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
+	public function getCreatedAt(): ?\DateTimeInterface
+	{
+		return $this->createdAt;
+	}
+
+	public function setCreatedAt(\DateTimeInterface $createdAt): self
+	{
+		$this->createdAt = $createdAt;
+
+		return $this;
+	}
+
+	public function getUpdatedAt(): ?\DateTimeInterface
+	{
+		return $this->updatedAt;
+	}
+
+	public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+	{
+		$this->updatedAt = $updatedAt;
+
+		return $this;
+	}
+
+	public function getDepartment(): ?Department
+	{
+		return $this->department;
+	}
+
+	public function setDepartment(?Department $department): self
+	{
+		$this->department = $department;
+
+		return $this;
+	}
+}
