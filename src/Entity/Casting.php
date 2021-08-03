4ddae661570afcdf8ddb3dd8dcee72ec417205d5
Oriane-Toque <oriane.toque@gmail.com>
2021-08-03 17:51:58 +0200

    api movies : all movies done

--- src/Entity/Casting.php
+++ src/Entity/Casting.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CastingRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=CastingRepository::class)
@@ -19,17 +20,20 @@ class Casting
 
 	/**
 	 * @ORM\Column(type="string")
+	 * @Groups("movies_get")
 	 */
 	private $role;
 
 	/**
 	 * @ORM\Column(type="integer")
+	 * @Groups("movies_get")
 	 */
 	private $creditOrder;
 
 	/**
 	 * @ORM\ManyToOne(targetEntity=Person::class, inversedBy="castings")
 	 * @ORM\JoinColumn(nullable=false)
+	 * @Groups("movies_get")
 	 */
 	private $person;
 
