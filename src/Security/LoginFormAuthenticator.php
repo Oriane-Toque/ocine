964e76299526c725c10f8844bfa0adc64f1db52f
Oriane-Toque <oriane.toque@gmail.com>
2021-11-26 15:02:05 +0100

    feature to sign up (front-office) done + fix subnav for back-office

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -51,7 +51,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('back_movie_browse'));
+        return new RedirectResponse($this->urlGenerator->generate('movie_list'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
